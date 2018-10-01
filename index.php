60d389a3814b2eccb2f6d8ebdccb1077a9cf247e
Fabian <fago@live.com>
2018-10-01 11:55:44 -0300

    update 2

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <span>hello world</span>
+  </body>
+</html>
